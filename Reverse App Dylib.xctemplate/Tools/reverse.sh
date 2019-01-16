ca13e4a1ec44dea62c8534d3289e9f5f6d9692ad
Jack <xuwenjiejack@gmail.com>
2019-01-16 10:05:06 +0800

    Fix for new shell

--- Reverse App Dylib.xctemplate/Tools/reverse.sh
+++ Reverse App Dylib.xctemplate/Tools/reverse.sh
@@ -16,29 +16,30 @@ des_tweak_dylib="${des_framewotk_path}/${tweak_dylib}"
 des_substitute_dylib="${des_macho_path}/${substitute_dylib}"
 des_macho="${des_macho_path}/${app_name}"
 
+# Backup origin macho
+if [ ! -f "${ori_macho}" ]; then
+    cp "${des_macho}" "${ori_macho}"
+fi
 
 rm -rf "$des_tweak_dylib"
-echo "XWJACK Remove ${des_tweak_dylib}"
 rm -rf "$des_substitute_dylib"
-echo "XWJACK Remove ${des_substitute_dylib}"
 rm -rf "$des_macho"
-echo "XWJACK Remove ${des_macho}"
 
 # Copy libsubstitute
 cp "${ori_substitute_dylib}" "${des_substitute_dylib}"
-echo "XWJACK Copy ${ori_substitute_dylib} to ${des_substitute_dylib}"
 
 # Copy Tweak
 cp "$ori_tweak_dylib" "$des_tweak_dylib"
-echo "XWJACK Copy ${ori_tweak_dylib} to ${des_tweak_dylib}"
 
 
 #Insert dylib/framework
 ${SRCROOT}/Tools/insert_dylib --all-yes "@loader_path/../Frameworks/${tweak_dylib}" "${ori_macho}" "${des_macho}"
 
-# Sign
-/usr/bin/codesign --verbose=4 -f -s "$CODE_SIGN_IDENTITY" "$des_substitute_dylib"
-/usr/bin/codesign --verbose=4 -f -s "$CODE_SIGN_IDENTITY" "$des_tweak_dylib"
-# Sign app
-/usr/bin/codesign --verbose=4 -f -s "$CODE_SIGN_IDENTITY" "${SRCROOT}/Resources/${app_name}.app"
 
+if [ CODE_SIGN_IDENTITY != "" ]; then
+    # Sign
+    /usr/bin/codesign --verbose=4 -f -s "$CODE_SIGN_IDENTITY" "$des_substitute_dylib"
+    /usr/bin/codesign --verbose=4 -f -s "$CODE_SIGN_IDENTITY" "$des_tweak_dylib"
+    # Sign app
+    /usr/bin/codesign --verbose=4 -f -s "$CODE_SIGN_IDENTITY" "${SRCROOT}/Resources/${app_name}.app"
+fi
--- Reverse App Framework.xctemplate/Tools/reverse.sh
+++ Reverse App Framework.xctemplate/Tools/reverse.sh
@@ -3,42 +3,43 @@ des_macho_path="${SRCROOT}/Resources/${app_name}.app/Contents/MacOS"
 des_framewotk_path="${SRCROOT}/Resources/${app_name}.app/Contents/Frameworks"
 CODE_SIGN_IDENTITY=""
 
-tweak_dylib="${TARGET_NAME}.framework"
+tweak_framework="${TARGET_NAME}.framework"
 substitute_dylib="libsubstitute.dylib"
 
 # 源文件
-ori_tweak_dylib="${BUILT_PRODUCTS_DIR}/${tweak_dylib}"
+ori_tweak_framework="${BUILT_PRODUCTS_DIR}/${tweak_framework}"
 ori_substitute_dylib="${SRCROOT}/${PROJECT_NAME}/Core/${substitute_dylib}"
-ori_macho="${SRCROOT}/Resources/${app_name}"
+ori_macho="${SRCROOT}/Resources/${app_name}_ori"
 
 # 目标文件
-des_tweak_dylib="${des_framewotk_path}/${tweak_dylib}"
+des_tweak_framework="${des_framewotk_path}/${tweak_framework}"
 des_substitute_dylib="${des_macho_path}/${substitute_dylib}"
 des_macho="${des_macho_path}/${app_name}"
 
 
-rm -rf "$des_tweak_dylib"
-echo "XWJACK Remove ${des_tweak_dylib}"
+if [ ! -f "${ori_macho}" ]; then
+    cp "${des_macho}" "${ori_macho}"
+fi
+
+rm -rf "$des_tweak_framework"
 rm -rf "$des_substitute_dylib"
-echo "XWJACK Remove ${des_substitute_dylib}"
 rm -rf "$des_macho"
-echo "XWJACK Remove ${des_macho}"
 
 # Copy libsubstitute
 cp "${ori_substitute_dylib}" "${des_substitute_dylib}"
-echo "XWJACK Copy ${ori_substitute_dylib} to ${des_substitute_dylib}"
 
 # Copy Tweak
-cp "$ori_tweak_dylib" "$des_tweak_dylib"
-echo "XWJACK Copy ${ori_tweak_dylib} to ${des_tweak_dylib}"
+cp "$ori_tweak_framework" "$des_tweak_framework"
 
 
 #Insert dylib/framework
-${SRCROOT}/Tools/insert_dylib --all-yes "@rpath/${tweak_dylib}" "${ori_macho}" "${des_macho}"
+${SRCROOT}/Tools/insert_dylib --all-yes "@rpath/${tweak_framework}/Versions/A/${PRODUCT_NAME}" "${ori_macho}" "${des_macho}"
 
-# Sign
-/usr/bin/codesign --verbose=4 -f -s "$CODE_SIGN_IDENTITY" "$des_substitute_dylib"
-/usr/bin/codesign --verbose=4 -f -s "$CODE_SIGN_IDENTITY" "$des_tweak_dylib"
-# Sign app
-/usr/bin/codesign --verbose=4 -f -s "$CODE_SIGN_IDENTITY" "${SRCROOT}/Resources/${app_name}.app"
 
+if [ CODE_SIGN_IDENTITY != "" ]; then
+    # Sign
+    /usr/bin/codesign --verbose=4 -f -s "$CODE_SIGN_IDENTITY" "$des_substitute_dylib"
+    /usr/bin/codesign --verbose=4 -f -s "$CODE_SIGN_IDENTITY" "$des_tweak_framework"
+    # Sign app
+    /usr/bin/codesign --verbose=4 -f -s "$CODE_SIGN_IDENTITY" "${SRCROOT}/Resources/${app_name}.app"
+fi
